22eb28a4b21d9925f96cd20a042279d66053e680
Eliot Courtney <edgeworth.dev@gmail.com>
2022-03-26 16:39:22 +0900

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    future_incompatible,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_abi,
+    nonstandard_style,
+    noop_method_call,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    trivial_casts,
+    unreachable_pub,
+    unsafe_code,
+    unsafe_op_in_unsafe_fn,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    unused
+)]
+#![allow(
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::items_after_statements,
+    clippy::many_single_char_names,
+    clippy::match_on_vec_items,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::module_name_repetitions,
+    clippy::similar_names,
+    clippy::struct_excessive_bools,
+    clippy::too_many_lines,
+    clippy::unreadable_literal
+)]
+
+pub mod lexer;
+pub mod parser;
+pub mod token;
+pub mod types;
